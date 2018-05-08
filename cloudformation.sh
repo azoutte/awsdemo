cf919c4decf45295ab582355b62fd5fd90f856ff
EC2 Default User <ec2-user@ip-172-31-45-209.us-east-2.compute.internal>
2018-05-08 17:26:01 +0000

    test

--- cloudformation.sh
+++ cloudformation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if aws cloudformation describe-stacks --stack-name MyStack > /dev/null; then
    aws cloudformation update-stack --stack-name MyStack --template-body file://./vpc.yaml
 else
